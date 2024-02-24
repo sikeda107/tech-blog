93f5a5113203c2500b289cb7a512d96a47146eda
sikeda107 <s.ikeda107@gmail.com>
2024-02-24 17:08:45 +0900

    Add INTERVAL variable to cloudbuild.yaml and waiting.sh

--- CloudBuildsTips/waiting.sh
+++ CloudBuildsTips/waiting.sh
@@ -8,11 +8,11 @@ IFS="," read -r BUILD_CREATE_TIME BUILD_TRIGGER_ID <<<"$QUERY_BUILD"
 
 FILTERS="id!=$CURRENT_BUILD_ID AND createTime<$BUILD_CREATE_TIME AND buildTriggerId=$BUILD_TRIGGER_ID"
 echo "Waiting for all builds to finish $FILTERS"
-INTERVALS=${INTERVALS:-5}
+INTERVAL=${INTERVAL:-5}
 while
   BUILDS_COUNT=$(gcloud builds list --ongoing --filter="$FILTERS" --format="value(id)" | wc -l)
   (( BUILDS_COUNT >= 1 ))
 do
-  echo "Current number of ongoing builds: $BUILDS_COUNT. Please wait a moment... Interval: $INTERVALS seconds."
-  sleep $INTERVALS
+  echo "Current number of ongoing builds: $BUILDS_COUNT. Please wait a moment... Interval: $INTERVAL seconds."
+  sleep $INTERVAL
 done
