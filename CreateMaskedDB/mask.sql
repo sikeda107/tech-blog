26815650b78d10fc1b2b8b70602e2486351d2df6
sikeda107 <s.ikeda107@gmail.com>
2024-03-03 16:00:31 +0900

    Add clone-instance and export-sql steps to cloudbuild_mask_db.yaml, update mask.sql

--- CreateMaskedDB/mask.sql
+++ CreateMaskedDB/mask.sql
@@ -1,2 +1,6 @@
-use world;
-select count(*) from city where countrycode = 'USA';
+SELECT * FROM city LIMIT 1;
+SELECT * FROM country LIMIT 1;
+UPDATE city SET Name = REPEAT('*', CHAR_LENGTH(Name));
+UPDATE country SET Name = REPEAT('*', CHAR_LENGTH(Name));
+SELECT * FROM city LIMIT 1;
+SELECT * FROM country LIMIT 1;
