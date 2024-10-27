78a65484146a344c6b07f230efd8379e285f2145
sikeda107 <s.ikeda107@gmail.com>
2024-10-27 17:58:11 +0900

    add test file

---
+++ PRAgentWithVertexAI/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const port = 8080;
+const requestHandler = (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World! from Server');
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+};
+const server = http.createServer(requestHandler);
+server.listen(port, () => {
+  console.log('Server is ready');
+  console.log(`Server listening on port ${port}`);
+});
